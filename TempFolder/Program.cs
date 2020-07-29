b71b65fb86b4e54203b27fff1dd815b6df119e8a
oguimbal <olivier.guimbal@gadz.org>
2020-07-29 09:40:09 +0000

    initial

---
+++ TempFolder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace TempFolder
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var prog = Path.Combine(Path.GetTempPath(), "TemporaryFolders");
+            if (!Directory.Exists(prog))
+                Directory.CreateDirectory(prog);
+            int i = 0;
+            string p;
+            while (Directory.Exists(p = Path.Combine(prog, i++.ToString())) && (Directory.EnumerateFiles(p).Any() || Directory.EnumerateDirectories(p).Any() )) { }
+            Directory.CreateDirectory(p);
+            Process.Start(p);
+        }
+    }
+}
